e5359f67ff82d3ef9de3676235db25fe05c12793
Winzlieb <winzlieb@c3d2.de>
2024-12-11 12:57:43 +0100

    feat: Add LiaScript editor URL and open command config options

--- src/core/config.py
+++ src/core/config.py
@@ -13,6 +13,8 @@ class PortalConfig(YAMLWizard):
     unit_root_path: str  # Path to unit directory (relative or absolute)
     liascript_devserver: str = "http://localhost:3000"
     liascript_html_path: str = "/liascript/index.html"
+    liascript_editor_url: str = "http://localhost:4321/?/tutor"
+    liascript_editor_open_command: str = "chromium --user-data-dir=~/.local/share/learning-platform/chromium-profile --app="
 
     @classmethod
     def load(cls) -> 'PortalConfig':
