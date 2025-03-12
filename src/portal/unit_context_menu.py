f837a9e897d8d290d5a73f979ed36b32ab6c2e7d
Winzlieb <winzlieb@c3d2.de>
2025-03-12 19:41:12 +0100

    feat: add "Open Folder" action to UnitContextMenu

--- src/portal/unit_context_menu.py
+++ src/portal/unit_context_menu.py
@@ -17,6 +17,14 @@ class UnitContextMenu(QMenu):
         editor_action = self.addAction(tr("Open in Editor"))
         editor_action.triggered.connect(self.open_in_editor)
         
+        folder_action = self.addAction(tr("Open Folder"))
+        folder_action.triggered.connect(self.open_folder)
+        
     def open_in_editor(self):
         command = self.config.liascript_editor_open_command.replace("%f", str(self.unit.relative_markdown_path))
         subprocess.Popen(command, shell=True)
+        
+    def open_folder(self):
+        folder_path = self.unit.markdown_path.parent
+        if folder_path.exists():
+            subprocess.Popen(['xdg-open', str(folder_path)])
